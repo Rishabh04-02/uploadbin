a18ad1bcc908ff00e2d09134a78861cda1d21bf1
Rishabh04-02 <rishabh0402@gmail.com>
2017-02-04 12:27:08 +0530

    admin files added

---
+++ sharing/admin/dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+//this is the dbconnect file
+//this will be used for all types of database connectivities
+//code below
+
+
+
+?>
---
+++ sharing/admin/login.php
@@ -0,0 +1,8 @@
+<?php
+//this is the file which will handle the login queries for the site whenever needed
+//code below
+
+
+
+
+?>
