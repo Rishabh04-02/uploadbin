050fbed2ade0cd5bac16ddb227b5dd10357f0fe5
Anurag-545 <ranaasr60@gmail.com>
2017-02-04 15:12:01 +0530

    register

--- sharing/dbconnect.php
+++ sharing/dbconnect.php
@@ -2,7 +2,13 @@
 //this is the dbconnect file
 //this will be used for all types of database connectivities
 //code below
+<?php
+define("host","localhost");
+define("uname","root");
+define("pass","anuraggaggu");
+define("dbname","projectbook");
 
+?>
 
 
 ?>
