7190dfadf83eaa15fc2182d392bb1bd7931de375
Rishabh04-02 <rishabh0402@gmail.com>
2017-02-04 12:08:34 +0530

    mail file added

---
+++ sharing/mail.php
@@ -0,0 +1,8 @@
+<?php
+//this is the mail.php file it will mail each and every chat message to the emailid of the concerned user
+//if the user doesn't respond in a fixed time interval
+//code below
+
+
+
+?>
