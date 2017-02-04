9b74d40388b06e61c5b9c126e7a644ebe56bba04
Rishabh04-02 <rishabh0402@gmail.com>
2017-02-04 11:39:21 +0530

    few pages created
    creation of few essential pages

---
+++ sharing/register.php
@@ -0,0 +1,8 @@
+<?php
+//this is the register file this will be used for the user registeration whenever needed
+//this will have recaptcha for validation
+//code below
+
+
+
+?>
---
+++ sharing/terms.php
@@ -0,0 +1,8 @@
+<?php
+//this is the file showing terms and conditions
+//this is just a formality
+//user can't publish a book without agreeing to the terms and conditions
+
+
+
+?>
