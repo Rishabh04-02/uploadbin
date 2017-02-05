be77352c47770315a8d548981a3bc7bd4a32d707
Rishabh04-02 <rishabh0402@gmail.com>
2017-02-05 09:10:21 +0530

    upload bin
    in progress

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Uploadbin</title>
+<link href="sharing/css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+<link href="sharing/css/style.css" type="text/css" rel="stylesheet" media="all">
+<!-- js -->
+<script src="sharing/js/jquery-1.11.1.min.js"></script> 
+   <script src="sharing/js/bootstrap.js"></script>
+<!-- //js -->
+</head>
+<body>
+<div class="container-liquid">
+<div class="row" id="header">
+     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" id="logo">
+      <img src="sharing/images/logo.png" id="logoimg"/><a href="index.php">Uploadbin</a>
+     </div>
+     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+     
+	 <form class="navbar-form navbar-left" role="search" id="search">
+  <div class="form-group">
+    <input type="text" class="form-control" placeholder="Search" id="in">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+<a href="sharing/"><button type="submit" class="btn btn-default">Bookhub</button></a>
+     </div>     
+</div>
+
+</div>
+</div>	
--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-This is the main upload page
+<?php
+//this is the main page this will include register.php and login.php
+//this can have the dynamic capacity to display the latest added books
+//code below
+
+include_once 'header.php';
+
+
+
+
+
+
+?>
+<center><br/>
+<form method="post" enctype="multipart/form-data">
+    Select file to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload File" name="submit">
+</form>
+</center>
+
+</body>
+</html>	
